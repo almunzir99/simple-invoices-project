221846f09ed3ef1a5c0344757a6a463e66b4e3a7
almunzir99 <almunzir99@gmail.com>
2022-12-02 11:19:22 +0200

    create repositories

---
+++ SimpleInvoicesProject_DAL/Repositories/InvoicesRepository.cs
@@ -0,0 +1,60 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using SimpleInvoicesProject_DAL.Database;
+using SimpleInvoicesProject_DAL.Interfaces;
+using SimpleInvoicesProject_DAL.Models;
+
+namespace SimpleInvoicesProject_DAL.Repositories;
+
+public class InvoicesRepository : IRepositoryBase<Invoice>
+{
+    private readonly AppDbContext _dbContext;
+
+    public InvoicesRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Invoice> Create(Invoice item)
+    {
+        await _dbContext.Invoices.AddAsync(item);
+        return item;
+    }
+
+    public async Task Delete(int id)
+    {
+        var target = await _dbContext.Invoices.FirstOrDefaultAsync(c => c.InvoiceId == id);
+        if (target == null)
+            throw new Exception("Target invoice isn't available");
+        _dbContext.Invoices.Remove(target);
+    }
+
+    public async Task<Invoice> Update(int id, Invoice item)
+    {
+        var target = await _dbContext.Invoices.AsNoTracking()
+            .FirstOrDefaultAsync(c => c.InvoiceId == id);
+        if (target == null)
+            throw new Exception("Target invoice isn't available");
+        item.InvoiceId = id;
+        target = item;
+        _dbContext.Invoices.Update(target);
+        return target;
+    }
+
+    public async Task<IList<Invoice>> List(Expression<Func<Invoice, bool>>? predicate)
+    {
+        var list = await _dbContext.Invoices.Where(predicate ?? (c => true)).ToListAsync();
+        return list;
+    }
+
+    public async Task<Invoice?> Single(int id)
+    {
+        var target = await _dbContext.Invoices
+            .FirstOrDefaultAsync(c => c.InvoiceId == id);
+        return target;
+    }
+    public async Task Complete()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+}
